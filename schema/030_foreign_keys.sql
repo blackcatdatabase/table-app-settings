062ac574207a517a2f71dc6371566c092a15d263
Tolben <vitek.pasek@gmail.com>
2025-10-21 23:57:46 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 on 2025-10-21T02:32:05
+-- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
 -- table: app_settings
 ALTER TABLE app_settings ADD CONSTRAINT fk_app_settings_user FOREIGN KEY (updated_by) REFERENCES users(id) ON DELETE SET NULL;
